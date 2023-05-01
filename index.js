ccfdb191302d38bff01d11364db6db80deba078b
Yonatan Salmon <yonatansalmon@itc.tech>
2023-05-01 12:50:45 +0300

    cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const supermarketRouter = require('./routes/supermarketRoutes');
 const app = express();
 const PORT = 8080;
 
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3000','https://housestuff.netlify.app'], credentials: true }));
 app.use(express.json());
 app.use('/api', supermarketRouter);
 
